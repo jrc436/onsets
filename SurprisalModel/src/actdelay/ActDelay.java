3aaeeeb284d1baa2ef45461fc39e8fae0ccfa53d
Jeremy Cole <jrc436@psu.edu>
2016-11-23 03:07:06 -0500

    had to add the file constructor specified

--- SurprisalModel/src/actdelay/ActDelay.java
+++ SurprisalModel/src/actdelay/ActDelay.java
@@ -16,4 +16,18 @@ public class ActDelay {
 	public String toString() {
 		return activation+","+delay;
 	}
+	public static ActDelay fromString(String s) {
+		String[] parts = s.split(",");
+		if (parts.length != 2) {
+			throw new IllegalArgumentException("String: "+s+" does not encode an actdelay, wrong number of commas");
+		}
+		try {
+			double activation = Double.parseDouble(parts[0]);
+			double delay = Double.parseDouble(parts[1]);
+			return new ActDelay(activation, delay);
+		}
+		catch (NumberFormatException nfe) {
+			throw new IllegalArgumentException("Error parsing actdelay, csv don't encod ea double: "+s);
+		}
+	}
 }
--- SurprisalModel/src/actdelay/ActDelayList.java
+++ SurprisalModel/src/actdelay/ActDelayList.java
@@ -1,5 +1,10 @@
 package actdelay;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 import util.collections.GenericList;
 import util.sys.DataType;
 
@@ -10,6 +15,32 @@ public class ActDelayList extends GenericList<ActDelay> {
 	}
 	public ActDelayList(ActDelayList other) {
 		super(other);
+	}
+	public ActDelayList(File f) {
+		List<String> lines = null;
+		try {
+			lines = Files.readAllLines(f.toPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("Error reading file: "+f);
+			System.exit(1);
+		}
+		boolean firstLine = true;
+		for (String line : lines) {
+			if (firstLine) {
+				firstLine = false;
+				continue;
+			}
+			this.add(ActDelay.fromString(line));
+		}
+	}
+	public static ActDelayList fromFile(File f) throws IOException {
+		List<String> lines = Files.readAllLines(f.toPath());
+		ActDelayList adl = new ActDelayList();
+		for (String line : lines) {
+			adl.add(ActDelay.fromString(line));
+		}
+		return adl;
 	}
 	private static final long serialVersionUID = 2666778742110702037L;
 
